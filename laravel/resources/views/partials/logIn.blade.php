bb23fcfbe6c5fed51e6943df268b3dff73a7bf43
Irish Manalo <altheairishmanalo@gmail.com>
2026-02-18 13:47:51 +0800

    Removed wrong file directory

--- laravel/resources/views/partials/logIn.blade.php
+++
@@ -1,9 +0,0 @@
-<div style= "border: 3px solid black;">
-    <h2>Log-in</h2>
-    <form action="/login" method="POST">
-        @csrf
-        <input type="text" name="loginname" placeholder="Name"><br><br>
-        <input type="password" name="loginpassword" placeholder="Password"><br><br>
-        <button type="submit">Log-in</button>
-    </form>
-</div>
